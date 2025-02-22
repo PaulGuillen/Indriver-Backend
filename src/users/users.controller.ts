bdf679a25de2e3f0e43b579174c44e65e1bf5f25
Paul Guillen <paulguillen190@gmail.com>
2025-02-21 20:14:07 -0600

    Create new user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private readonly usersService: UsersService) { }
+    
+    @Post()
+    create(@Body() user: CreateUserDTO) {
+        return this.usersService.create(user);
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(
+        @InjectRepository(User) private readonly usersRepository: Repository<User>
+    ) { }
+
+    create(user: CreateUserDTO){
+        const newUser = this.usersRepository.create(user);
+        return this.usersRepository.save(newUser);
+    }
+}
