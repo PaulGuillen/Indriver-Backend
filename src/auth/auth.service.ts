3eba0ccde45cf12ec5c8dc14b0c6b3df3095c6ed
Paul Guillen <paulguillen190@gmail.com>
2025-02-21 20:48:01 -0600

    Encrypt password and validate email and phone if exists

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/users/user.entity';
 import { Repository } from 'typeorm';
 import { RegisterUserDTO } from './dto/register-user.dto';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
 export class AuthService {
 
-    constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) { }
+    constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) {}
 
-    register(user: RegisterUserDTO) {
+    async register(user: RegisterUserDTO) {
+        if (!user.email || !user.phone) {
+            throw new HttpException('Email and phone are required', HttpStatus.BAD_REQUEST);
+        }
+        const [emailExist, phoneExist] = await Promise.all([
+            this.usersRepository.findOneBy({ email: user.email }),
+            this.usersRepository.findOneBy({ phone: user.phone })
+        ]);
+
+        if (emailExist) {
+            throw new HttpException('Email already exists', HttpStatus.CONFLICT);
+        }
+
+        if (phoneExist) {
+            throw new HttpException('Phone already exists', HttpStatus.CONFLICT);
+        }
+
         const newUser = this.usersRepository.create(user);
-        return this.usersRepository.save(newUser);
+
+        try {
+            return await this.usersRepository.save(newUser);
+        } catch (error) {
+            throw new HttpException('Error while registering user', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-} 
+}
