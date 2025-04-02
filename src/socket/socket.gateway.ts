84f115d73831103f4e12324fa1747617e46814ab
Paul Guillen <paulguillen190@gmail.com>
2025-04-01 19:08:25 -0600

    Listen and emit information in real time socket io

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,17 +1,17 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
 
 @WebSocketGateway({
     cors: {
         origin: "*",
     },
-    transports : ["websocket"],
+    transports: ["websocket"],
 })
 
 export class SocketGateway extends Server implements OnGatewayConnection, OnGatewayDisconnect {
 
     @WebSocketServer()
-    server : Server;
+    server: Server;
 
     handleDisconnect(client: Socket) {
         console.log("Client disconnected: ", client.id);
@@ -21,4 +21,16 @@ export class SocketGateway extends Server implements OnGatewayConnection, OnGate
         console.log("Client connected: ", client.id);
     }
 
+    @SubscribeMessage("message")
+    handleMessage(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+        console.log("Message received: ", data);
+        client.emit("message", 'Bien gracias');
+    }
+
+    @SubscribeMessage("change_driver_position")
+    handleChangeDriverPosition(@ConnectedSocket() client: Socket, @MessageBody() data: any) {
+      console.log("Nuevas posicion: ", data);
+      client.emit("new_driver_position", { id: data.id, lat: data.lat, lng: data.lng });
+    }
+    
 }
